7bde16b310ae3dc2083292c7e21e42cf5e0b2840
panyayan311 <levanha711@gmail.com>
2017-02-21 22:02:22 +0900

    upload files

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,22 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by levanha711 on 2017/02/21.
+//  Copyright © 2017 BlueStanford. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect)
+    {
+        var skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        var skullCenter = center
+        
+        
+    }
+
+}
---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by levanha711 on 2017/02/21.
+//  Copyright © 2017 BlueStanford. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+
+
+}
+
